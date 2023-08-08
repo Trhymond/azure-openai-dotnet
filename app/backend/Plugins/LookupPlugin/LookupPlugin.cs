e8840690c87e60529adb2cfd139403b9ec42926a
Tomy Rhymond <tomy.rhymond@slalom.com>
2023-08-08 11:22:24 -0400

    updates

--- app/backend/Plugins/LookupPlugin/LookupPlugin.cs
+++ app/backend/Plugins/LookupPlugin/LookupPlugin.cs
@@ -5,14 +5,22 @@ public sealed class LookupPlugin {
     private readonly SearchClient _searchClient;
     private readonly RequestOverrides? _requestOverrides;
 
-    public LookupPlugin(SearchClient searchClient)
+    public LookupPlugin(SearchClient searchClient, RequestOverrides? requestOverrides)
     {
         _searchClient = searchClient;
+        _requestOverrides = requestOverrides;
     }
 
     [SKFunction, Description("Query Azure Cognitive Search")]
-    public async Task<string> QueryAsync(string searchQuery)
+    public async Task<string> LookupAsync(string searchQuery, SKContext context)
     {
-        return await Task.FromResult(searchQuery);
+        if (lookupQuery is string query)
+        {
+            return await _searchClient.LookupAsync(query, _requestOverrides);
+        }
+
+        throw new AIException(
+            AIException.ErrorCodes.ServiceError,
+            "Query skill failed to get query from context");
     }
 }
