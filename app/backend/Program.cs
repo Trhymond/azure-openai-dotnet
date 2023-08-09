6fe25cf0fdac662706ace6174f57a4b5de7fd5c1
Tomy Rhymond <trhymond@gmail.com>
2023-08-09 09:23:00 -0400

    updats

--- app/backend/Program.cs
+++ app/backend/Program.cs
@@ -2,6 +2,23 @@ using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .Build();
+    .ConfigureServices(s=> {
+        s.AddApplicationInsightsTelemetryWorkerService();
+        s.ConfigureFunctionsApplicationInsights();
+        s.AddSingleton<IHttpResponderService, DefaultHttpResponderService>();
+        s.Configure<LoggerFilterOptions>(options => {
+            LoggerFilterRule? toRemove = options.Rules.FirstOrDefault(rule => rule?.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsigthsLoggerProvider");
+            
+            if(toRemove is not null) {
+                options.Rules.Remove(toRemove);
+            }
+        });
+        s.AddLogging();
+        s.AddDistributedMemoryCache();
+        s.AddAzureServices();
+        s.AddMemoryStore();
+    }).Build();
 
-host.Run();
+
+await host.RunAsync();
