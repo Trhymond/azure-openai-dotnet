72406e08053c15b1c4a79180b130c2ecc54a0a31
tomy.rhymond <tomy.rhymond@slalom.com>
2023-08-10 13:38:52 -0400

    updates

--- app/backend/Models/ApproachResponse.cs
+++ app/backend/Models/ApproachResponse.cs
@@ -1,8 +1,27 @@
 namespace Rhymond.OpenAI.Models;
 
-public record ApproachResponse(
-    string Answer,
-    string? Thoughts,
-    string[] DataPoints,
-    string CitationBaseUrl,
-    string? Error = null);
+// public record ApproachResponse(
+//     string Answer,
+//     string? Thoughts,
+//     string[] DataPoints,
+//     string CitationBaseUrl,
+//     string? Error = null);
+
+
+public record class ApproachResponse
+{
+    [JsonProperty("answer")]
+    public required string Answer { get; set; }
+
+    [JsonProperty("thoughts")]
+    public Approach Thoughts { get; set; }
+
+    [JsonProperty("data_points")]
+    public string[] DataPoints { get; set; }
+
+    [JsonProperty("citation_base_url")]
+    public string CitationBaseUrl { get; set; }
+
+    [JsonProperty("error")]
+    string? error { get; set; } = ""
+}
